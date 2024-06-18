3e9f7b1040f4a5bca857d153f799a755144f9840
Steve De Jongh <de.jongh.steve@gmail.com>
2024-06-18 13:41:33 -0700

    Back end devise sign up login log out

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem "devise-api", "~> 0.1.3"
+gem "devise-jwt"
+gem "jsonapi-serializer"
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,12 @@
+class UserSerializer
+  include JSONAPI::Serializer
+  attributes :id, :email, :created_at
+
+  attribute :created_date do |user|
+    user.created_at && user.created_at.strftime('%m/%d/%Y')
+  end
+end
+
+# UserSerializer.new(resource).serializable_hash[:data][:attributes].to_json
+# And multiple records by,
+# UserSerializer.new(resource).serializable_hash[:data].map{|data| data[:attributes].to_json}
