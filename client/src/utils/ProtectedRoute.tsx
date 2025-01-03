78247128400a84472f37160e5e10e6b9e5860419
Steve De Jongh <de.jongh.steve@gmail.com>
2025-01-03 14:52:01 -0800

    Protected route improvements to navigation on reject

--- client/src/utils/ProtectedRoute.tsx
+++ client/src/utils/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { RoleTypes } from "../types/users";
 import { useAuth } from "../contexts/AuthContext";
+import { useEffect } from "react";
 
 type Props = {
   role: RoleTypes;
@@ -9,17 +10,19 @@ type Props = {
 function ProtectedRoute({ role }: Props) {
   const navigate = useNavigate();
   const { user } = useAuth();
-  console.log("User from protectedRoute", user);
 
-  if (!user) {
-    console.error("No user signed in.");
-    return <Navigate to="/login" replace />;
-  }
+  useEffect(() => {
+    console.log("User in hook", user);
+    if (!user) {
+      navigate("/login");
+      return;
+    }
 
-  if (!user.roles?.includes(role as RoleTypes)) {
-    console.error("User doesn't have that role.");
-    return <Navigate to={navigate(-1)} />;
-  }
+    if (user && !user.roles?.includes(role as RoleTypes)) {
+      navigate(-1);
+      return;
+    }
+  });
 
   return <Outlet />;
 }
